f3311c4aa6bfc188f253c2f4e19c18ac8a686caa
formuhan <syw1034455910@163.com>
2019-05-31 16:47:15 +0800

    获取糗事百科图片信息

--- qiushi/qiushi/pipelines.py
+++ qiushi/qiushi/pipelines.py
@@ -4,8 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class QiushiPipeline(object):
+    def __init__(self):
+        self.filename=open('images.json','wb')
     def process_item(self, item, spider):
+        json_text=json.dumps(dict(item),ensure_ascii=False)+',\n'
+        self.filename.write(json_text.encode('utf-8'))
         return item
+    def close_spider(self,spider):
+        self.filename.close()
